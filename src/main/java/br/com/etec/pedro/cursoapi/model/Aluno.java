8ceeb8c646f7f971147ac20f5b5b16584015926e
Pedro Henrique <pedrohvribeiro@gmail.com>
2023-03-15 19:49:50 -0300

    migrations

--- src/main/java/br/com/etec/pedro/cursoapi/model/Aluno.java
+++ src/main/java/br/com/etec/pedro/cursoapi/model/Aluno.java
@@ -1,14 +1,17 @@
 package br.com.etec.pedro.cursoapi.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+
 
 @Entity
-@Table(name = "aluno")
+@Table(name = "Aluno")
+
 public class Aluno {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
 
   private Long id;
   private String nomeAluno;
--- src/main/java/br/com/etec/pedro/cursoapi/model/Curso.java
+++ src/main/java/br/com/etec/pedro/cursoapi/model/Curso.java
@@ -3,15 +3,10 @@ package br.com.etec.pedro.cursoapi.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "curso")
+@Table(name = "Curso")
 public class Curso {
 
   @Id
